17fe47631ff5b660ddf527393e2116c336ef501a
KevinC <kpcamacho@hotmail.com>
2017-03-07 17:16:04 -0500

    Changed .env.example

---
+++ database/migrations/2017_03_07_212728_create_itemset_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ItemSet', function (Blueprint $table) {
+           $table->increments('userID');
+           $table->string('itemSetID');
+           $table->string('championID');
+           $table->string('item1ID');
+           $table->string('item2ID');
+           $table->string('item3ID');
+           $table->string('item4ID');
+           $table->string('item5ID');
+           $table->string('item6ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ItemSet');
+    }
+}
