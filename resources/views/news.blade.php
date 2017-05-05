f57c15af414313f8f571e4f7da4f02ede4d7a2f0
Greydon Whittaker Kingsley <gwkingsl@lcs-208-159.syr.edu>
2017-05-05 16:00:12 -0400

    Added comments to the news.blade.php view.

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
-  <title> League Report News </title>
+  <title> League Report News</title>
     
     <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -26,19 +26,20 @@
   <body>
 <!-- Main Container -->
 <div class="container" style="padding-left: 0; padding-right: 0;"> 
-  <!-- Navigation -->
+  <!-- Navigation: Creates the links to the other pages on our site to make navigating easy. 
+  The links to the HOME,ITEM SET BUILD, NEWS, and LOGIN pages are all made here-->
   <header>
   <h4 class="logo"><img src="{{asset('images/logo.png')}}" width="50" height="50" alt=""/> LEAGUE REPORT</h4>
     <nav>
       <ul>
-        <li><a href="/">HOME</a></li>
+        <li><a href="/">HOME</a></li> 
         <li><a href="/item">ITEM SET BUILD</a></li>
         <li><a href="/news">NEWS</a></li>
         <li><a href="/login">LOGIN</a></li>
       </ul>
     </nav>
   </header>
-  <!--News Section -->
+  <!--News Section. This is where entire code lies that creates our news page's content. -->
 <section class="news" id="news" style="background-image: url({{asset('images/wall1.jpg')}})">
 <div style="float: right;">
 <form onsubmit="go_get(); return false;" >
@@ -48,51 +49,52 @@
 
 <iframe id="youriframe" width="1000" height="600" allowfullscreen="true" src="http://www.youtube.com/embed?listType=search&list=leagueoflegendsnews"></iframe>  
 </div> 
-<!-- start sw-rss-feed code --> 
+<!-- start sw-rss-feed code. Sets up the look and feel of the RSS news feed we have on the news page. --> 
 <script type="text/javascript"> 
 <!-- 
-rssfeed_url = new Array(); 
-rssfeed_url[0]="http://na.leagueoflegends.com/en/rss.xml";  
-rssfeed_frame_width="600"; 
-rssfeed_frame_height="420"; 
-rssfeed_scroll="off"; 
-rssfeed_scroll_step="6"; 
-rssfeed_scroll_bar="on"; 
-rssfeed_target="_blank"; 
-rssfeed_font_size="12"; 
-rssfeed_font_face=""; 
-rssfeed_border="on"; 
-rssfeed_css_url="http://feed.surfing-waves.com/css/style6.css"; 
-rssfeed_title="on"; 
-rssfeed_title_name="League Of Legends Official News"; 
-rssfeed_title_bgcolor="#ff0000"; 
-rssfeed_title_color="#fff"; 
-rssfeed_title_bgimage="http://"; 
-rssfeed_footer="off"; 
-rssfeed_footer_name="rss feed"; 
-rssfeed_footer_bgcolor="#fff"; 
-rssfeed_footer_color="#333"; 
-rssfeed_footer_bgimage="http://"; 
-rssfeed_item_title_length="50"; 
-rssfeed_item_title_color="#666"; 
-rssfeed_item_bgcolor="#fff"; 
-rssfeed_item_bgimage="http://"; 
-rssfeed_item_border_bottom="on"; 
-rssfeed_item_source_icon="off"; 
-rssfeed_item_date="off"; 
-rssfeed_item_description="on"; 
-rssfeed_item_description_length="120"; 
-rssfeed_item_description_color="#666"; 
-rssfeed_item_description_link_color="#333"; 
-rssfeed_item_description_tag="off"; 
-rssfeed_no_items="0"; 
-rssfeed_cache = "ceb30d669b8bad85720dc26d4fea70f1"; 
+  rssfeed_url = new Array(); 
+  rssfeed_url[0]="http://na.leagueoflegends.com/en/rss.xml";  
+  rssfeed_frame_width="600"; 
+  rssfeed_frame_height="420"; 
+  rssfeed_scroll="off"; 
+  rssfeed_scroll_step="6"; 
+  rssfeed_scroll_bar="on"; 
+  rssfeed_target="_blank"; 
+  rssfeed_font_size="12"; 
+  rssfeed_font_face=""; 
+  rssfeed_border="on"; 
+  rssfeed_css_url="http://feed.surfing-waves.com/css/style6.css"; 
+  rssfeed_title="on"; 
+  rssfeed_title_name="League Of Legends Official News"; 
+  rssfeed_title_bgcolor="#ff0000"; 
+  rssfeed_title_color="#fff"; 
+  rssfeed_title_bgimage="http://"; 
+  rssfeed_footer="off"; 
+  rssfeed_footer_name="rss feed"; 
+  rssfeed_footer_bgcolor="#fff"; 
+  rssfeed_footer_color="#333"; 
+  rssfeed_footer_bgimage="http://"; 
+  rssfeed_item_title_length="50"; 
+  rssfeed_item_title_color="#666"; 
+  rssfeed_item_bgcolor="#fff"; 
+  rssfeed_item_bgimage="http://"; 
+  rssfeed_item_border_bottom="on"; 
+  rssfeed_item_source_icon="off"; 
+  rssfeed_item_date="off"; 
+  rssfeed_item_description="on"; 
+  rssfeed_item_description_length="120"; 
+  rssfeed_item_description_color="#666"; 
+  rssfeed_item_description_link_color="#333"; 
+  rssfeed_item_description_tag="off"; 
+  rssfeed_no_items="0"; 
+  rssfeed_cache = "ceb30d669b8bad85720dc26d4fea70f1"; 
 //--> 
 </script> 
 <script type="text/javascript" src="http://feed.surfing-waves.com/js/rss-feed.js"></script> 
 <!-- The link below helps keep this service FREE, and helps other people find the SW widget. Please be cool and keep it! Thanks. --> 
 <div style="text-align:right; width:600px;"><a href="http://www.surfing-waves.com/feed.htm" target="_blank" style="color:#ccc;font-size:10px">widget @</a> <a href="http://www.surfing-waves.com" target="_blank" style="color:#ccc;font-size:10px">surfing-waves.com</a></div> 
-<!-- end sw-rss-feed code -->
+<!-- end sw-rss-feed code.-->
+<!--This is where I add the links to other popular League of Legends News websites/blogs. There are 4 different links: Riot Official news page, TheRiftHerald, Surrender@20, and the reddit league of legends subreddit.-->
  <form action="http://na.leagueoflegends.com/en/news/">
     <input type="image" style="height:75px;width: 150px"  src="http://stepbystepguides.org/wp-content/uploads/2017/02/League-of-legends.jpg"/>
 </form>
@@ -105,6 +107,8 @@ rssfeed_cache = "ceb30d669b8bad85720dc26d4fea70f1";
 <form action="https://www.reddit.com/r/leagueoflegends/">
     <input type="image" style="height:75px;width: 150px" src="http://marketingland.com/wp-content/ml-loads/2014/07/reddit2-1920.jpg" />
 </form>
+
+<!--This is where I add the youtube player to the news page. I create an iFrame then make requests to the youtube api and data bind the data I get back to the iFrame. -->
 <script type="text/javascript">
 
 function go_get(){
@@ -114,7 +118,7 @@ function go_get(){
  var ifr = document.getElementById('youriframe');
  ifr.src = target_url;
  return false;
-}
+} 
 </script> 
 </section>
  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
